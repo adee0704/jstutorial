aeef3d837fe2cdf671b1759fd7a1855286493179
adee0704 <81531994+adee0704@users.noreply.github.com>
2024-08-15 07:17:40 +0000

    Data types

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -5,4 +5,4 @@ let islogged = false
 let name ="awara hoon ya gardish me hi asman ka tara hoon"
 let wife = null
 let enemy = undefined
-console.table([typeof 3+3,typeof islogged, islogged, typeof wife, typeof enemy])
+console.table([typeof 3+3,typeof islogged, islogged, typeof wife, typeof enemy,typeof name , name])
