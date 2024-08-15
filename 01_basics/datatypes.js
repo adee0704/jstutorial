e1d7908c8866a42c8889656da60879f17d74d818
adee0704 <81531994+adee0704@users.noreply.github.com>
2024-08-15 07:13:58 +0000

    Container config

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+"Use Strict"
+const a = 12345
+console.log(typeof a)
+let islogged = false
+let name ="awara hoon ya gardish me hi asman ka tara hoon"
+let wife = null
+let enemy = undefined
+console.table([typeof 3+3,typeof islogged, islogged, typeof wife, typeof enemy])
--- 01_basics/dee.js
+++ 01_basics/dee.js
@@ -1,1 +1,6 @@
+const accountid =12345
+let accountemail ="deepak@gmail.com" 
+let accountcity ="bengaluru"
+console.table([accountid,accountemail,accountcity])
 console.log("Deepak")
+
